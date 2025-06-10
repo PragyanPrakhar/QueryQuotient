a6afabc509086e2d9de611de794cef6b2acf734d
Pragyan Prakhar <pragyanprakhar@gmail.com>
2025-06-10 17:07:29 +0530

    Configured Cookies, Checked and validated inputs and bugs resolve

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,26 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../models/user.model.js";
 import { inngest } from "../inngest/client.js";
 
 export const signup = async (req, res) => {
     const { username, email, password, skills = [] } = req.body;
     try {
+        if (!email || !username || !password) {
+            return res.status(400).json({
+                error: "Email, username and password are required",
+            });
+        }
+        // Check if the user already exists
+        const existingUser = await User.findOne({
+            email,
+        });
+        if (existingUser) {
+            return res.status(400).json({
+                error: "User already exists with this email",
+            });
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await User.create({
             username,
@@ -14,11 +29,13 @@ export const signup = async (req, res) => {
             skills,
         });
 
+        const savedUser = await user.save();
+
         // Now we will trigger the inngest event
         // the use of await is to ensure that the event is processed before sending the response
-        // this is not necessary but it is a good practice to ensure that the event is processed before sending the response
-        // if you want to send the response immediately, you can remove the await keyword
-        // and the event will still be processed in the background
+        // this is not necessary buresponse immediately, you can remove the await keyword
+        // and the event will stillt it is a good practice to ensure that the event is processed before sending the response
+        // if you want to send the  be processed in the background
         await inngest.send({
             name: "user/signup",
             data: {
@@ -29,10 +46,18 @@ export const signup = async (req, res) => {
         console.log("📨 Event sent: user/signup");
 
         const token = jwt.sign(
-            { _id: user._id, role: user.role },
+            { _id: savedUser._id, role: savedUser.role },
             process.env.JWT_SECRET
         );
-        res.json({
+        // Set the token in cookies
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production", // Set to true in production
+            sameSite: "Strict", // Helps prevent CSRF attacks
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
+        });
+        res.status(201).json({
+            message: "User signed up successfully",
             user: {
                 _id: user._id,
                 username: user.username,
@@ -53,6 +78,11 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
     const { email, password } = req.body;
     try {
+        if (!email || !password) {
+            return res.status(400).json({
+                error: "Email and password are required",
+            });
+        }
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(404).json({
@@ -69,7 +99,13 @@ export const login = async (req, res) => {
             { _id: user._id, role: user.role },
             process.env.JWT_SECRET
         );
-        res.json({
+        res.cookie("token",token,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production", // Set to true in production
+            sameSite: "Strict", // Helps prevent CSRF attacks
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
+        })
+        res.status(201).json({
             user: {
                 _id: user._id,
                 username: user.username,
@@ -89,21 +125,22 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        const token = req.headers.authorization.split(" ")[1]; // Invalidate the token by setting it to null
+        /* const token = req.headers.authorization.split(" ")[1]; // Invalidate the token by setting it to null
         if (!token) {
             return res.status(400).json({
                 error: "No token provided",
             });
-        }
-        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        } */
+        /* jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
             if (err) {
                 return res.status(401).json({
                     error: "Invalid token",
                 });
             }
-        });
+        }); */
+        res.clearCookie("token");
 
-        res.json({
+        return res.status(200).json({
             message: "User logged out successfully",
         });
     } catch (error) {
@@ -117,6 +154,8 @@ export const logout = async (req, res) => {
 export const updateUser = async (req, res) => {
     const { skills = [], role, email } = req.body;
     try {
+        
+
         if (req.user?.role !== "admin") {
             return res.status(403).json({
                 error: "You are not authorized to update user",
@@ -161,7 +200,7 @@ export const getUsers = async (req, res) => {
             });
         }
         const users = await User.find({}, "-password").sort({ createdAt: -1 });
-        res.json(users);
+        return res.status(200).json({users});
     } catch (error) {
         res.status(500).json({
             message: "Error getting users",
